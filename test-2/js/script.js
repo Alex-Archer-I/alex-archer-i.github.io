d21f04092517e896951069477cf59be156e7f12e
Alex <archertrail@outlook.com>
2021-03-10 21:50:43 +0300

    fixed wrong js file

--- test-2/js/script.js
+++ test-2/js/script.js
@@ -1,118 +1,55 @@
-window.addEventListener("DOMContentLoaded", () => {
-
-    //
-
-    function classChange(element, oldClass, newClass) {
-        element.classList.remove(oldClass);
-        element.classList.add(newClass);
+$(document).ready(function() {
+    $('.slider').slider({
+        range: 'min',
+        max: 255,
+        value: 230,
+        slide: getRGB   
     }
-    
-    function multipleSlider(className, classActive, classHide, length, forward) {
-    
-        const elements = document.querySelectorAll(className);
-        let left;
-        let right;
-    
-        elements.forEach((n, i) => {
-            if (n.classList.contains(`${classActive}_1`)) {
-                left = elements[i - 1];
-                right = elements[i + length];
-            }
-        });
-    
-        if (forward) {
-            if (document.querySelectorAll(`.${classHide}_left`).length == 0) {
-                return;
-            }
-    
-            for (let i = length; i > 0; i--) {
-                if (i == length) {
-                    classChange(document.querySelector(`.${classActive}_${i}`), `${classActive}_${i}`, `${classHide}_right`);
-                    continue;
-                }
-    
-                classChange(document.querySelector(`.${classActive}_${i}`), `${classActive}_${i}`, `${classActive}_${i + 1}`);
-            }
-    
-            classChange(left, `${classHide}_left`, `${classActive}_1`);
+    );
+
+    let key = 'background-color';
+    let bgc = [230, 230, 230];
+    let txc = [0, 0, 0];
+
+    $('.toggle-bg').on('click', () => {
+        if (key == 'color') {
+            key = 'background-color';
+            setValue(bgc);
+            $('.toggle-bg').toggleClass('active');
+            $('.toggle-tx').toggleClass('active');
+        }
+    });
+
+    $('.toggle-tx').on('click', () => {
+        if (key == 'background-color') {
+            key = 'color';
+            setValue(txc);
+            $('.toggle-tx').toggleClass('active');
+            $('.toggle-bg').toggleClass('active');
+        }
+    });
+
+    function setValue(arr) {
+        $('.slider-1').slider('value', arr[0]);
+        $('.slider-2').slider('value', arr[1]);
+        $('.slider-3').slider('value', arr[2]);
+    }
+
+    function getRGB() {
+        if (key === 'background-color') {
+            bgc[0] = $('.slider-1').slider('value');
+            bgc[1] = $('.slider-2').slider('value');
+            bgc[2] = $('.slider-3').slider('value');
+
+            $('.color').css(`${key}`, `rgb(${bgc[0]}, ${bgc[1]}, ${bgc[2]})`);
         } else {
-            if (document.querySelectorAll(`.${classHide}_right`).length == 0) {
-                return;
-            }
-    
-            for (let i = 1; i <= length; i++) {
-                if (i == 1) {
-                    classChange(document.querySelector(`.${classActive}_${i}`), `${classActive}_${i}`, `${classHide}_left`);
-                    continue;
-                }
-    
-                classChange(document.querySelector(`.${classActive}_${i}`), `${classActive}_${i}`, `${classActive}_${i - 1}`);
-            }
-    
-            classChange(right, `${classHide}_right`, `${classActive}_${length}`);
+            txc[0] = $('.slider-1').slider('value');
+            txc[1] = $('.slider-2').slider('value');
+            txc[2] = $('.slider-3').slider('value');
+
+            $('.color').css(`${key}`, `rgb(${txc[0]}, ${txc[1]}, ${txc[2]})`);
         }
     }
 
-    function random(min, max) {
-        return Math.floor(Math.random() * (max - min) + min);
-    }
-
-    //
-
-    const sliderOne = document.querySelector('.slider-one');
-    const sliderTwo = document.querySelector('.slider-two');
-    const sliderThree = document.querySelector('.slider-three');
-    const sliderClassic = document.querySelector('.slider-classic');
-
-    sliderOne.addEventListener('click', (event) => {
-        if (event.target.classList.contains('arrow-one_left')) {
-            if (document.querySelectorAll(`.hide-one_right`).length != 0) {
-                multipleSlider('.image-one', 'active-one', 'hide-one', 4, false);
-
-                for (let i = 1; i < 5; i++) {
-                    const n = random(-15, 15);
-                    const e = document.querySelector(`.active-one_${i}`);
-                    e.style.transform = `rotate(${n}deg)`;
-                }
-            }
-        }
-        if (event.target.classList.contains('arrow-one_right')) {
-            if (document.querySelectorAll(`.hide-one_left`).length != 0) {
-                multipleSlider('.image-one', 'active-one', 'hide-one', 4, true);
-
-                for (let i = 1; i < 5; i++) {
-                    const n = random(-15, 15);
-                    const e = document.querySelector(`.active-one_${i}`);
-                    e.style.transform = `rotate(${n}deg)`;
-                }
-            }
-        }
-    });
-
-    sliderTwo.addEventListener('click', (event) => {
-        if (event.target.classList.contains('arrow-two_left')) {
-            multipleSlider('.image-two', 'active-two', 'hide-two', 3, false);
-        }
-        if (event.target.classList.contains('arrow-two_right')) {
-            multipleSlider('.image-two', 'active-two', 'hide-two', 3, true);
-        }
-    });
-
-    sliderThree.addEventListener('click', (event) => {
-        if (event.target.classList.contains('arrow-three_left')) {
-            multipleSlider('.image-three', 'active-three', 'hide-three', 5, false);
-        }
-        if (event.target.classList.contains('arrow-three_right')) {
-            multipleSlider('.image-three', 'active-three', 'hide-three', 5, true);
-        }
-    });
-
-    sliderClassic.addEventListener('click', (event) => {
-        if (event.target.classList.contains('arrow-classic_left')) {
-            multipleSlider('.image-classic', 'active', 'hide-classic', 1, false);
-        }
-        if (event.target.classList.contains('arrow-classic_right')) {
-            multipleSlider('.image-classic', 'active', 'hide-classic', 1, true);
-        }
-    });
+    getRGB();
 });
